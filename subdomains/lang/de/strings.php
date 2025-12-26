45378a02727f59e3da6af180eb1bc35831880bc8
JuggleFredJr <mail@philippstamp.de>
2025-12-26 16:35:24 +0100

    added german translation

---
+++ subdomains/lang/de/strings.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'no_domains' => 'Keine Domains',
+    'domain' => 'Domain|Domains',
+
+    'no_subdomains' => 'Keine Subdomains',
+    'subdomain' => 'Subdomain|Subdomawins',
+    'limit' => 'Limit',
+    'change_limit' => 'Limit anpassen',
+    'limit_changed' => 'Limit angepasst',
+    'limit_reached' => 'Subdomainlmit erreicht',
+    'create_subdomain' => 'Subdomain erstellen',
+
+    'name' => 'Name',
+
+    'api_token' => 'Cloudflare API Token',
+    'api_token_help' => 'Das Token benötigt Leseberechtigung für Zone.Zone und Schreibberechtigung für Zone.Dns. Für eine verstärkte Sicherheit können mit "Zone Resources" bestimmte Domains ausgeschlossen werden. Mit "Client IP Adress Filtering" kannst du die Panel-IP freigeben.',
+];
