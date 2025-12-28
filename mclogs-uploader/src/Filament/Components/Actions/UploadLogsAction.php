abe6bccca61f17386ff662328e53b68078870a52
Felix <154911483+zfelyx@users.noreply.github.com>
2025-12-28 04:32:14 +0100

    Add egg tag check to determine log upload availability

--- mclogs-uploader/src/Filament/Components/Actions/UploadLogsAction.php
+++ mclogs-uploader/src/Filament/Components/Actions/UploadLogsAction.php
@@ -22,10 +22,53 @@ class UploadLogsAction extends Action
         parent::setUp();
 
         $this->hidden(function () {
+            $mc_tag = "mclogs-updater";
+
             /** @var Server $server */
             $server = Filament::getTenant();
 
-            return $server->retrieveStatus()->isOffline();
+            if ($server->retrieveStatus()->isOffline()) {
+                return true;
+            }
+            $egg = $server->egg;
+
+            // check if egg has tag 'mclogs-updater'
+            if (method_exists($egg, $mc_tag)) {
+                try {
+                    $hasTag = $egg->tags()->where('name', $mc_tag)->exists();
+                    if ($hasTag) {
+                        return false;
+                    }
+                } catch (Exception $e) {
+                }
+            }
+            if (isset($egg->tags)) {
+                $tags = is_string($egg->tags) ? json_decode($egg->tags, true) : $egg->tags;
+                if (is_array($tags) && in_array($mc_tag, $tags)) {
+                    return false;
+                }
+            }
+            if (isset($egg->meta) && is_array($egg->meta) && isset($egg->meta['tags'])) {
+                if (in_array($mc_tag, $egg->meta['tags'])) {
+                    return false;
+                }
+            }
+
+            try {
+                $eggTags = \DB::table('egg_tag')
+                    ->join('tags', 'egg_tag.tag_id', '=', 'tags.id')
+                    ->where('egg_tag.egg_id', $egg->id)
+                    ->where('tags.name', $mc_tag)
+                    ->exists();
+
+                if ($eggTags) {
+                    return false;
+                }
+            } catch (Exception $e) {
+
+            }
+
+            return true;
         });
 
         $this->label(fn () => trans('mclogs-uploader::upload.upload_logs'));
