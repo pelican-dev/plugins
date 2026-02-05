65561c92d3f3b449a8294a9fd6a0e03a7699ccf3
Kingly Shade <150642678+KinglyShade@users.noreply.github.com>
2026-02-04 18:16:29 -0600

    Refactor MinecraftLoader enum and fromTags method

--- minecraft-modrinth/src/Enums/MinecraftLoader.php
+++ minecraft-modrinth/src/Enums/MinecraftLoader.php
@@ -8,19 +8,19 @@ use Illuminate\Support\Str;
 
 enum MinecraftLoader: string implements HasLabel
 {
-    case NeoForge   = 'neoforge';
-    case Forge      = 'forge';
-    case Fabric     = 'fabric';
-    case Quilt      = 'quilt';
-    case Paper      = 'paper';
-    case Purpur     = 'purpur';
-    case Folia      = 'folia';
+    case NeoForge = 'neoforge';
+    case Forge = 'forge';
+    case Fabric = 'fabric';
+    case Quilt = 'quilt';
+    case Paper = 'paper';
+    case Purpur = 'purpur';
+    case Folia = 'folia';
     case Pufferfish = 'pufferfish';
-    case Spigot     = 'spigot';
-    case Bukkit     = 'bukkit';
-    case Velocity   = 'velocity';
+    case Spigot = 'spigot';
+    case Bukkit = 'bukkit';
+    case Velocity = 'velocity';
     case Bungeecord = 'bungeecord';
-    case Waterfall  = 'waterfall';
+    case Waterfall = 'waterfall';
 
     /**
      * Get the label for the loader.
@@ -43,40 +43,33 @@ enum MinecraftLoader: string implements HasLabel
         return self::fromTags($tags);
     }
 
-    /**
-     * Get the loader instance from an array of tags.
-     *
-     * @param  string[]  $tags
-     * @return self|null
-     */
-   public static function fromTags(array $tags): ?self
-{
-    if (! in_array('minecraft', $tags)) {
-        return null;
+    public static function fromTags(array $tags): ?self
+    {
+        if (! in_array('minecraft', $tags)) {
+            return null;
+        }
+
+        return match (true) {
+            // Mod Loaders
+            in_array('neoforge', $tags) || in_array('neoforged', $tags) => self::NeoForge,
+            in_array('forge', $tags) => self::Forge,
+            in_array('fabric', $tags) => self::Fabric,
+            in_array('quilt', $tags) => self::Quilt,
+
+            // Server Software (Forks)
+            in_array('folia', $tags) => self::Folia,
+            in_array('purpur', $tags) => self::Purpur,
+            in_array('pufferfish', $tags) => self::Pufferfish,
+            in_array('paper', $tags) || in_array('papermc', $tags) => self::Paper,
+            in_array('spigot', $tags) || in_array('spigotmc', $tags) => self::Spigot,
+            in_array('bukkit', $tags) => self::Bukkit,
+
+            // Proxy Software
+            in_array('velocity', $tags) => self::Velocity,
+            in_array('waterfall', $tags) => self::Waterfall,
+            in_array('bungeecord', $tags) || in_array('bungee', $tags) => self::Bungeecord,
+
+            default => null,
+        };
     }
-
-    return match (true) {
-        // Mod Loaders
-        in_array('neoforge', $tags) || in_array('neoforged', $tags) => self::NeoForge,
-        in_array('forge', $tags)                                   => self::Forge,
-        in_array('fabric', $tags)                                  => self::Fabric,
-        in_array('quilt', $tags)                                   => self::Quilt,
-
-        // Server Software (Forks)
-        in_array('folia', $tags)                                   => self::Folia,
-        in_array('purpur', $tags)                                  => self::Purpur,
-        in_array('pufferfish', $tags)                              => self::Pufferfish,
-        in_array('paper', $tags) || in_array('papermc', $tags)     => self::Paper,
-        in_array('spigot', $tags) || in_array('spigotmc', $tags)   => self::Spigot,
-        in_array('bukkit', $tags)                                  => self::Bukkit,
-
-        // Proxy Software
-        in_array('velocity', $tags)                                => self::Velocity,
-        in_array('waterfall', $tags)                               => self::Waterfall,
-        in_array('bungeecord', $tags) || in_array('bungee', $tags) => self::Bungeecord,
-
-        default => null,
-    };
-}
-
 }
