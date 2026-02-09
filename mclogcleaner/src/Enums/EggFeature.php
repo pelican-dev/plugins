5b82b62fdab703477c0efb53678beb02738580d3
Philipp Stamp <mail@philippstamp.de>
2026-02-09 14:58:14 +0100

    Fixed some things coderrabbitai wanted me to change :D

--- mclogcleaner/src/Enums/EggFeature.php
+++ mclogcleaner/src/Enums/EggFeature.php
@@ -4,14 +4,15 @@ namespace JuggleGaming\McLogCleaner\Enums;
 
 use App\Models\Server;
 
-enum EggFeature: string {
+enum EggFeature: string
+{
     case Check = 'mclogcleaner';
 
     public static function fromServer(Server $server): ?self
     {
         $server->loadMissing('egg');
 
-        $features = $server->egg?->features ?? [];
+        $features = $server->egg->features ?? [];
 
         if (in_array(self::Check->value, $features, true)) {
             return self::Check;
