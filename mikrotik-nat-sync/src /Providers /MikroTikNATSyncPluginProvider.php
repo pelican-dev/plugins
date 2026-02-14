308d646ad87f5626e6310a73ea54ede544150574
avalon0077 <43924346+avalon0077@users.noreply.github.com>
2026-02-14 17:43:22 +0200

    Add MikroTikNATSyncPluginProvider service provider

---
+++ mikrotik-nat-sync/src /Providers /MikroTikNATSyncPluginProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Avalon\MikroTikNATSync\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MikroTikNATSyncPluginProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
