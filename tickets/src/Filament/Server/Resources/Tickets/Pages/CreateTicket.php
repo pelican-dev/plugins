0996e9418a7a584e1d5ed90a6b7e60f0386efb55
DamonOne <alparka155@gmail.com>
2026-02-24 12:03:18 +0200

    Add form actions for creating and canceling tickets
    Quick fix for CreateTicket in server area.

--- tickets/src/Filament/Server/Resources/Tickets/Pages/CreateTicket.php
+++ tickets/src/Filament/Server/Resources/Tickets/Pages/CreateTicket.php
@@ -4,6 +4,7 @@ namespace Boy132\Tickets\Filament\Server\Resources\Tickets\Pages;
 
 use Boy132\Tickets\Filament\Server\Resources\Tickets\TicketResource;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Actions\Action;
 
 class CreateTicket extends CreateRecord
 {
@@ -14,5 +15,22 @@ class CreateTicket extends CreateRecord
     public function getBreadcrumbs(): array
     {
         return [];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+            Action::make('create')
+                ->label(__('filament-panels::resources/pages/create-record.form.actions.create.label'))
+                ->submit('create')
+                ->color('primary')
+                ->button(),
+            
+            Action::make('cancel')
+                ->label(__('filament-panels::resources/pages/create-record.form.actions.cancel.label'))
+                ->url($this->getResource()::getUrl())
+                ->color('gray')
+                ->button(),
+        ];
     }
 }
