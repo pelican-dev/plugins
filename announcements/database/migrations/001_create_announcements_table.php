6125565c43cc1302e7f5b6b9fd2af3b55b200baf
drkthunder02 <mancuso.christopher@gmail.com>
2026-05-11 19:20:41 -0500

    Added Schema check in Announcements plugin.

--- announcements/database/migrations/001_create_announcements_table.php
+++ announcements/database/migrations/001_create_announcements_table.php
@@ -8,16 +8,18 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('announcements', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->string('body')->nullable();
-            $table->string('type')->default('info');
-            $table->json('panels')->nullable();
-            $table->timestamp('valid_from')->nullable();
-            $table->timestamp('valid_to')->nullable();
-            $table->timestamps();
-        });
+        if(!Schema::hasTable('announcements')) {
+            Schema::create('announcements', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('title');
+                $table->string('body')->nullable();
+                $table->string('type')->default('info');
+                $table->json('panels')->nullable();
+                $table->timestamp('valid_from')->nullable();
+                $table->timestamp('valid_to')->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     public function down(): void
